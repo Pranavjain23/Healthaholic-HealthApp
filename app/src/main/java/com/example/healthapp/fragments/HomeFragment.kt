21195b0cd5dc8cbcf3d228c822ec09b1e4051b65
Bhoomika <bhoomika.garg.9@gmail.com>
2021-01-02 17:52:07 +0530

    UI changes

--- app/src/main/java/com/example/healthapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/healthapp/fragments/HomeFragment.kt
@@ -1,19 +1,17 @@
 package com.example.healthapp.fragments
 
-import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.fragment.app.Fragment
 import androidx.cardview.widget.CardView
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.healthapp.R
-import com.example.healthapp.activity.StepCounterActivity
 import com.example.healthapp.activity.BmiActivity
 import com.example.healthapp.activity.DevelopedByActivity
-import com.example.healthapp.activity.MainActivity
+import com.example.healthapp.activity.StepCounterActivity
+
 
 class HomeFragment : Fragment() {
 
@@ -39,10 +37,11 @@ class HomeFragment : Fragment() {
             val intent = Intent(context, BmiActivity::class.java)
            startActivity(intent)
         }
+
         return view
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_information,menu)
+        inflater.inflate(R.menu.menu_information, menu)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.info){
